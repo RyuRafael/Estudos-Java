619fbe8a3ef152751ea2d7428bc3e19ed76414cd
Rafael Ribeiro Rodrigues <rafaelribeirorodrigues84@gmail.com>
2026-03-23 18:39:15 -0300

    FileWriter e BufferedWriter

--- Arquivos/LendoArquivosDeTexto/src/application/Main.java
+++ Arquivos/LendoArquivosDeTexto/src/application/Main.java
@@ -1,31 +1,54 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		String path = "/home/rafael/Documentos/good.txt";
+		String[] lines = new String[] {"Rafael","Bob","Maria"};
+		Scanner sc = new Scanner(System.in);
+		//Caminho que quer criar um novo arquivo
+		String path = "/home/rafael/Documentos/nomes.txt";
 		
-		// Agora o BufferedReader e o FileReader são instanciados dentro da declaração do bloco try
-		// Fecha automaticamente o BufferedReader e o FileReader
-		try (BufferedReader br = new BufferedReader( new FileReader(path))){
+		// Instancia o objeto
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
 			
-			String file = br.readLine();
+			System.out.println("Digite a quantidade de nomes que você quer escrever: ");
+			int n = sc.nextInt();
+			sc.nextLine();
 			
-			while(file != null) {
-				System.out.println(file);
-				file = br.readLine();
+			for(int i = 1; i <= n; i++) {
+				System.out.println("Digite o nome #" + i + ": ");
+				String nome = sc.nextLine();
+				// Escreve o nome digitado no arquivo
+				// Também posso passar um lista de nomes: bw.write(lines);
+				bw.write(nome);
+				// Faz uma quebra de linha
+				bw.newLine();
 			}
-			
 		}
-		catch (IOException e) {
-			System.out.println("ERRO: " + e.getMessage());
+		catch (Exception e) {
+			System.out.println("Erro " + e.getMessage());
+		}
+		
+		// Ler o arquivo
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Erro: " + e.getMessage());
 		}
 
 	}
