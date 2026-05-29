90c35f487a850c1743f67a55e3c350a817fb7d7c
Rafael Ribeiro Rodrigues <rafaelribeirorodrigues84@gmail.com>
2026-05-29 13:17:59 -0300

    Expressão lambda declarada

--- Programacao-funcional-expressoes-lambda/Consumer/Exemplo/src/application/Main.java
+++ Programacao-funcional-expressoes-lambda/Consumer/Exemplo/src/application/Main.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 /*Fazer um programa que, a partir de uma lista de produtos, aumente o
 preço dos produtos em 10%.*/
@@ -24,7 +24,9 @@ public class Main {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::priceUpdate);
+		Consumer<Product> cons = p -> p.setPreco(p.getPreco() * 1.1);
+		
+		list.forEach(cons);
 
 		list.forEach(System.out::println);
 	}
