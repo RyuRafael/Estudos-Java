05aa0f5b2b370257d1567835acad12bfd4a5017f
Rafael Ribeiro Rodrigues <rafaelribeirorodrigues84@gmail.com>
2026-06-05 16:38:58 -0300

    Inserir dados

--- Jdbc/jdbc1/src/application/Main.java
+++ Jdbc/jdbc1/src/application/Main.java
@@ -1,15 +1,20 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		Connection conn = null;
 		// Preparar um comando sql
@@ -20,6 +25,17 @@ public class Main {
 		try {
 			// Conectar com o banco
 			conn = DB.getConnection();
+			// Inserir dados
+			st = conn.prepareStatement(
+					"INSERT INTO seller" + "(Name, Email, BirthDate, BaseSalary, DepartmentId)" + "Values (?,?,?,?,?)");
+
+			st.setString(1, "Rafael Ribeiro");
+			st.setString(2, "rafaelribeirorodrigues82@gmail.com");
+			st.setDate(3, new Date(sdf.parse("03/09/2003").getTime()));
+			st.setDouble(4, 4800);
+			st.setInt(5, 1);
+			st.executeUpdate();
+
 			// Fazer pesquisa
 			st = conn.prepareStatement("select * from department");
 			rs = st.executeQuery();
@@ -29,7 +45,7 @@ public class Main {
 				System.out.println(rs.getInt("id") + ", " + rs.getString("Name"));
 			}
 
-		} catch (SQLException e) {
+		} catch (SQLException | ParseException e) {
 			e.printStackTrace();
 		} finally {
 			DB.closeConnection();
